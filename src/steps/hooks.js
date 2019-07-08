963a7719febd9beb349a9671561e67c2292e3076
Kevin Roulleau <kevin.roulleau@happn.com>
2019-07-08 12:23:08 +0200

    test tagged and async hooks

--- src/steps/hooks.js
+++ src/steps/hooks.js
@@ -3,13 +3,25 @@
 
 const { Before, After } = require('cucumber');
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
+
 Before(function () {
     this.count = 0;
     console.log('[DEBUG] - Cucumber Before hook');
 });
 
-After(function (context) {
+After(async function (context) {
     this.count += 1;
     console.log('[DEBUG] - Cucumber After hook');
+
+    console.time('test');
+    await sleep(100);
+    console.timeEnd('test');
+
     // console.log('Result:', context.result.status);
+});
+
+After({ tags: '@test' }, function () {
+    this.tag = '@test';
+    console.log('[DEBUG] - Cucumber After hook with tag @test');
 });
